53291aad65418196f03cd49a18350c3d21b0caf8
gkcsd <kaleg2025@gmail.com>
2021-07-02 22:35:06 +0530

    Conatct comp added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,69 +7,67 @@ import { Row, Col } from "reactstrap";
 import { FaRegStar, FaStar } from "react-icons/fa";
 import { MdDelete, MdEdit } from "react-icons/md";
 
-//TODO: DONE, add firebase
+//TODO: DONE add firebase
 import firebase from "firebase/app";
 
 // context stuffs
-//TODO: DONE, import context and action: update and single_contact
+//TODO: DONE import context and action: update and single_contact
 import { ContactContext } from "../context/Context";
-import {CONTACT_TO_UPDATE, SET_SINGLE_CONTACT} from "../context/action.types";
+import { CONTACT_TO_UPDATE, SET_SINGLE_CONTACT } from "../context/action.types";
 
 import { useHistory } from "react-router-dom";
 
 import { toast } from "react-toastify";
 
-
 const Contact = ({ contact, contactKey }) => {
-  //TODO: DONE, destructuring dispatch from the context
-  const {dispatch} = useContext(ContactContext);
+  //TODO: DONE destructuring dispatch from the context
+  const { dispatch } = useContext(ContactContext);
 
   // history hooks to get history
   const history = useHistory();
 
   // to delete the contact when delete contact is clicked
   const deleteContact = () => {
-    //TODO: DONE, create this method from firebase
-    firebase.database()
+    //TODO: DONE create this method from firebase
+    firebase
+      .database()
       .ref(`/contacts/${contactKey}`)
       .remove()
       .then(() => {
-        toast("Deleted Successfully", {
-          type: "error"
-        })
+        toast("Deleted Successfully", { type: "warning" });
       })
-      .catch(err => console.log(err))
+      .catch(err => console.log(err));
   };
 
   // update the star/important contact ,ie, star it or unstar the single contact
   const updateImpContact = () => {
-    //TODO: DONE, update (star) contact, use contactKey
-    firebase.database()
+    //TODO: DONE update (star) contact, use contactKey
+    firebase
+      .database()
       .ref(`/contacts/${contactKey}`)
-      .update({
-        star: !contact.star
-      },
-      err => {
-        console.log(err)
-      }
+      .update(
+        {
+          star: !contact.star
+        },
+        err => {
+          console.log(err);
+        }
       )
       .then(() => {
-        toast("Contact Updated", {
-          type: "info"
-        })
+        toast("Contact Updated", { type: "info" });
       })
-      .catch(err => console.log(err))
+      .catch(err => console.log(err));
   };
 
   // when the update icon/ pen ion is clicked
   const updateContact = () => {
     // dispatching one action to update contact
-    //TODO: DONE, use dispatch to update
+    //TODO: DONE use dispatch to update
     dispatch({
       type: CONTACT_TO_UPDATE,
       payload: contact,
       key: contactKey
-    })
+    });
 
     // and pushing to the add contact screen
     history.push("/contact/add");
@@ -78,11 +76,11 @@ const Contact = ({ contact, contactKey }) => {
   // to view a single contact in the contact/view screen
   const viewSingleContact = contact => {
     // setting single contact in state
-    //TODO: DONE, use dispatch to view single contact
+    //TODO: use dispatch to view single contact
     dispatch({
       type: SET_SINGLE_CONTACT,
       payload: contact
-    })
+    });
 
     // sending...
     history.push("/contact/view");
@@ -113,9 +111,7 @@ const Contact = ({ contact, contactKey }) => {
           <div className="text-primary">{contact.name}</div>
 
           <div className="text-secondary">{contact.phoneNumber}</div>
-          <div className="text-secondary">
-            {contact.email}
-          </div>
+          <div className="text-secondary">{contact.email}</div>
 
           <div className="text-info">{contact.address}</div>
         </Col>
