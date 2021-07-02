e1278d0c564d469718b656c16623175e94e453e6
gkcsd <kaleg2025@gmail.com>
2021-07-02 22:37:04 +0530

    div section added

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -19,12 +19,12 @@ const Contacts = () => {
   // handle fab icon button click
   // will set in state of the contact to update and send it to the contact/add route
   const AddContact = () => {
-    //TODO: DONE, use dispatch to send user to add contact screen
+    //TODO: use dispatch to send user to add contact screen
     dispatch({
       type: CONTACT_TO_UPDATE,
       payload: null,
       key: null
-    })
+    });
     history.push("/contact/add");
   };
 
@@ -42,16 +42,16 @@ const Contacts = () => {
     <Container className="mt-4">
       {/* TODO: Loop through FIREBASE objects  */}
       {contacts.length === 0 && !isLoading ? (
-        <div className="center text-large text-primary">No Contacts Found In Firebase</div>
+        <div className="Center text-large text-primary">
+          NO Contacts found in firebase
+        </div>
       ) : (
         <ListGroup>
-          {
-            Object.entries(contacts).map(([key, value]) => {
-              <ListGroupItem key={key}>
-                <Contact contact={value} contactKey={key} />
-              </ListGroupItem>
-            })
-          }
+          {Object.entries(contacts).map(([key, value]) => (
+            <ListGroupItem key={key}>
+              <Contact contact={value} contactKey={key} />
+            </ListGroupItem>
+          ))}
         </ListGroup>
       )}
       <MdAdd className="fab icon " onClick={AddContact} />
